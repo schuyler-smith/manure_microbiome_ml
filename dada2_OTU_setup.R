d7b4ba3ee57c9153c810145909ea789b308a02ae
sdsmith1390 <sdsmith@iastate.edu>
2018-11-06 15:29:40 -0600

    complete runthrough of dada2 pipeline locally

--- dada2_OTU_setup.R
+++ dada2_OTU_setup.R
@@ -9,6 +9,7 @@ sapply(c(.cran_packages, .bioc_packages), require, character.only = TRUE)
 source("create_pan_microbiome.R")
 source("../phyloseq_scripts/find_phyloseq_generalists.R")
 set.seed(1390)
+cores = 4
 
 path <- "~/Desktop/da2_test/unassembled"
 fnFs <- sort(list.files(path, pattern="_R1_", full.names = TRUE)); fnRs <- sort(list.files(path, pattern="_R2_", full.names = TRUE))
@@ -17,20 +18,23 @@ filtFs <- file.path(path, "filtered", paste0(sample.names, "_F_filt.fastq.gz"));
 names(filtFs) <- sample.names; names(filtRs) <- sample.names
 # out <- filterAndTrim(fwd = fnFs, filt = filtFs, rev = fnRs, filt.rev = filtRs, truncLen=c(240,200), maxLen = 260, truncQ=2, rm.phix=TRUE, compress=TRUE, multithread=TRUE) # On Windows set multithread=FALSE
 out <- readRDS("data/out.RDS")
-# errF <- learnErrors(filtFs, multithread=TRUE, randomize = TRUE);errR <- learnErrors(filtRs, multithread=TRUE, randomize = TRUE)
+# errF <- learnErrors(filtFs, multithread=cores, randomize = TRUE);errR <- learnErrors(filtRs, multithread=cores, randomize = TRUE)
 errF <- readRDS("data/errF.RDS");errR <- readRDS("data/errR.RDS")
 merged_reads <- vector("list", length(sample.names)); names(merged_reads) <- sample.names
+merged_reads <- readRDS("data/merged_reads.RDS")
 
 for(sample in sample.names) {
   if(sample == sample.names[1]){n = 1}
   derepFs <- derepFastq(filtFs[[sample]], verbose = FALSE); derepRs <- derepFastq(filtRs[[sample]], verbose = FALSE)
-  dadaFs <- dada(derepFs, err = errF, multithread = TRUE, verbose = FALSE)
-  dadaRs <- dada(derepRs, err = errR, multithread = TRUE, verbose = FALSE)
+  dadaFs <- dada(derepFs, err = errF, multithread = cores, verbose = FALSE)
+  dadaRs <- dada(derepRs, err = errR, multithread = cores, verbose = FALSE)
   merged_reads[[sample]] <- mergePairs(dadaFs, derepFs, dadaRs, derepRs, verbose = FALSE)
-  print(paste("Sample",sample,n,"of",number_samples,"(",(round(n/number_samples,4)*100),"%) processed.",format(object.size(merged_reads), units = "GB"), sep = " "));n = n + 1
+  print(paste("Sample",sample,n,"of",number_samples,"(",(round(n/number_samples,4)*100),"%) processed.", sep = " "));n = n + 1
 }
 
 seqtab <- makeSequenceTable(merged_reads)
+seqtab <- removeBimeraDenovo(seqtab, method="consensus", multithread=cores)
 
+tax <- assignTaxonomy(seqtab, "~/Desktop/da2_test/database/current_Bacteria_unaligned.fa.gz", multithread=cores)
 
-
+# format(object.size(merged_reads), units = "GB")
