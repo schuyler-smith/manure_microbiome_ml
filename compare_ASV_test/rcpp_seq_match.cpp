6886e9be5996275241cdaad22cc16eb190d5d4f9
Schuyler Smith <sdsmith@iastate.edu>
2018-11-20 09:17:07 -0600

    update seq match for faster loops

--- compare_ASV_test/rcpp_seq_match.cpp
+++ compare_ASV_test/rcpp_seq_match.cpp
@@ -13,22 +13,33 @@ using namespace std;
 
 // [[Rcpp::export]]
 
-Rcpp::StringVector match_seqs(Rcpp::StringVector input1, Rcpp::StringVector input2){
+Rcpp::StringVector match_seqs(Rcpp::StringVector s_input, Rcpp::StringVector l_input){
 
-    int size1 = input1.size();
-    int size2 = input2.size();
+    int s_size = s_input.size();
+    int l_size = l_input.size();
 
-    for(int i=0; i<size1; ++i){
-    	string seq1 (input1[i]);
-    	for(int j=0; j<size2; ++j){
-    		string seq2 (input2[j]);
-    		if (seq2.find(seq1) != std::string::npos) {
-    			input2[j] = seq1;
-			}
-    	}
+   //  for(int i=0; i<s_size; ++i){
+   //  	string s_seq (s_input[i]);
+   //  	for(int j=0; j<l_size; ++j){
+   //  		string l_seq (l_input[j]);
+   //  		if (l_seq.find(s_seq) != std::string::npos) {
+   //  			l_input[j] = s_seq;
+			// }
+   //  	}
+   //  }
+    for(int i=0; i<l_size; ++i){
+        string l_seq (l_input[i]);
+        for(int j=0; j<s_size; ++j){
+            string s_seq (s_input[j]);
+            if (l_seq.find(s_seq) != std::string::npos) {
+                l_input[i] = s_seq;
+                break;
+            }
+        }
     }
 
-    return(input2);
+
+    return(l_input);
 }
 
 
--- compare_ASV_test/scripts.R
+++ compare_ASV_test/scripts.R
@@ -1,21 +1,21 @@
 # (...){
   options(warn=1)
   require(phyloseq);require(Rcpp)
-  sourceCpp("rcpp_seq_match.cpp")
+  sourceCpp("compare_ASV_test/rcpp_seq_match.cpp")
   
   phyloseq_objects <- list(...)
   p_objects_names <- sapply(substitute(list(...))[-1], deparse)
 
-  phyloseq_objects <- list(readRDS("~/Dropbox/compare_ASV_test/pitfoam.RDS"), readRDS("~/Dropbox/compare_ASV_test/nashua.RDS"))
+  phyloseq_objects <- list(readRDS("~/Dropbox/manure_ml/compare_ASV_test/pitfoam.RDS"), readRDS("~/Dropbox/manure_ml/compare_ASV_test/nashua.RDS"))
   p_objects_names <- c("pitfoam", "nashua")
   names(phyloseq_objects) <- p_objects_names
 
-  asvs <- lapply(phyloseq_objects, FUN=function(x){taxa_names(x)})
+  asvs <- lapply(phyloseq_objects, taxa_names)
   read_size_order <- order(unlist(lapply(lapply(asvs, `[[`, 1), FUN = function(seq){mean(length(strsplit(seq, "")[[1]]))})))
   asvs <- asvs[read_size_order]
   
   pairs <- combn(names(asvs), m = 2)
-
+  
   for(run in 1:dim(pairs)[2]){
     # asvs[[pairs[,run][2]]] <- match_seqs(asvs[[pairs[,run][1]]], asvs[[pairs[,run][2]]])
     test <- match_seqs(asvs[[pairs[,run][1]]], asvs[[pairs[,run][2]]])
