92de9f66e1db89d1187262d43aa0385a28e794c6
Schuyler Smith <sdsmith@iastate.edu>
2018-11-22 08:31:11 -0600

    working on combining duplicates

--- compare_ASV_test/rcpp_seq_match_test.cpp
+++ compare_ASV_test/rcpp_seq_match_test.cpp
@@ -4,35 +4,47 @@
  * 
  *      Author: Schuyler Smith
  */
-#include "Rcpp.h"
 #include <iostream>
 #include <string>
 #include <regex>
-
+#include <RcppArmadillo.h>
+// [[Rcpp::depends(RcppArmadillo)]]
 using namespace std;
 
 // [[Rcpp::export]]
 
 Rcpp::DataFrame test_match(Rcpp::NumericMatrix s_input, Rcpp::NumericMatrix l_input){
 
-    Rcpp::List s_names = s_input.attr("dimnames"); 
-    Rcpp::List l_names = l_input.attr("dimnames"); 
-    Rcpp::StringVector shorter_seqs = s_names[0];
-    Rcpp::StringVector longer_seqs = l_names[0];
+    Rcpp::StringVector shorter_seqs = rownames(s_input);
+    Rcpp::StringVector longer_seqs = rownames(l_input);
     int s_size = s_input.nrow();
     int l_size = l_input.nrow();
 
-    for(int i=0; i<l_size; ++i){
-        string l_seq (longer_seqs[i]);
-        for(int j=0; j<s_size; ++j){
-            string s_seq (shorter_seqs[j]);
-            if (l_seq.find(s_seq) != std::string::npos) {
-                longer_seqs[i] = s_seq;
-                break;
+    vector<int> dup_rows;
+    int ind; //create index counter for original of duplicate seq names
+    for(int i=0; i<s_size; ++i){ //loop through each sequence of set with shorter read lengths
+        string s_seq (shorter_seqs[i]);
+        ind = -1; //assign ind to not be within the dataframe
+        for(int j=0; j<l_size; ++j){ //loop through other dataset
+            string l_seq (longer_seqs[j]);
+            if (l_seq.find(s_seq) != std::string::npos){ //if seq 1 is contained in seq 2
+                longer_seqs[j] = s_seq;
+                if (ind == -1){
+                    ind = j;
+                } else {
+                    l_input(ind,Rcpp::_) = l_input(ind,Rcpp::_) + l_input(j,Rcpp::_); //add values to original
+                    dup_rows.push_back(j); //keep track of inidices of duplicated seqs
+                }
             }
         }
     }
-
+    std::sort(dup_rows.begin(), dup_rows.end()); //sort the indices to iterate through
+    arma::mat y = Rcpp::as<arma::mat>(l_input);
+    int dup_size = dup_rows.size();
+    for(int r=0; r<dup_size; ++r){
+        y.shed_row(dup_rows[r]-r);
+        // longer_seqs.erase(dup_rows[r]-r);
+    }
     
     return longer_seqs;
 }
--- compare_ASV_test/write_match.R
+++ compare_ASV_test/write_match.R
@@ -5,7 +5,19 @@ require(Rcpp)
 
 
 
-sourceCpp("compare_ASV_test/rcpp_seq_match_test.cpp")
-test_match(asvs[[pairs[,run][1]]][1:5,1:5], asvs[[pairs[,run][2]]][1:6,1:5])
+sourceCpp("compare_ASV_test/rcpp_seq_match_test.cpp");test_match(asvs[[pairs[,run][1]]][1:2694,], asvs[[pairs[,run][2]]][1:2694,])
+dup_rows <- test_match(asvs[[pairs[,run][1]]][1:2694,], asvs[[pairs[,run][2]]][1:2694,])
+test <- test_match(asvs[[pairs[,run][1]]][1:2694,], asvs[[pairs[,run][2]]][1:2694,])
+c(dup_rows)
+unlist(c(asvs[[pairs[,run][2]]][226,]))
+test[2265,]
+unlist(c(test[2,1:10]))
+unlist(c(test[2350,1:10]))
+unlist(c(test[2694,1:10]))
+unlist(c(test[4193,1:10]))
 
-asvs[[pairs[,run][1]]][1:5,1:5][,1]
+
+### Find which rows are repeated, test to see if they combine abundances correctly
+
+a <- test_match(asvs[[pairs[,run][1]]], asvs[[pairs[,run][2]]])
+                
