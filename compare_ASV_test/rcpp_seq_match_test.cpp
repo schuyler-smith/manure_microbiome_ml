46988e8456e25fc4af9017c7654121a5eed2b07e
Schuyler Smith <sdsmith@iastate.edu>
2018-11-23 20:45:56 -0600

    improve dynamic programming

--- compare_ASV_test/rcpp_seq_match_test.cpp
+++ compare_ASV_test/rcpp_seq_match_test.cpp
@@ -20,16 +20,17 @@ using namespace std;
 Rcpp::DataFrame test_match(Rcpp::NumericMatrix short_input, Rcpp::NumericMatrix long_input){
 
     Rcpp::StringVector shorter_seqs = rownames(short_input);
-    Rcpp::StringVector longer_seqs = rownames(long_input);
+    Rcpp::StringVector longer_seqs = rownames(clone(long_input));
     arma::mat l_input = Rcpp::as<arma::mat>(clone(long_input));
     int s_size = short_input.nrow();
     int l_size = long_input.nrow();
 
 
     vector<double> dup_rows;
-    vector<double> first_dup_rows;
+    vector<double> og_seqs;
     int ind; //create index counter for original of duplicate seq names
     // #pragma omp parallel for shared(long_input)
+    #pragma omp parallel for shared(l_input)
     for(int i=0; i<s_size; ++i){ //loop through each sequence of set with shorter read lengths
         string s_seq (shorter_seqs[i]);
         ind = -1; //assign ind to not be within the dataframe
@@ -40,28 +41,27 @@ Rcpp::DataFrame test_match(Rcpp::NumericMatrix short_input, Rcpp::NumericMatrix 
                 if (ind == -1){
                     ind = j;
                 } else {
-                    first_dup_rows.push_back(ind);
+                    og_seqs.push_back(ind);
                     dup_rows.push_back(j); //keep track of inidices of duplicated seqs
                 }
             }
         }
     }
-    // std::sort(dup_rows.begin(), dup_rows.end(), std::greater<int>()); //sort the indices in reverse order to iterate through
-    // int dup_size = dup_rows.size();
-    // for(int r=0; r<dup_size; ++r){ //remove data rows and duplicate taxa names
-    //     l_input.row(ind) = l_input.row(ind) + l_input.row(j); //add values to original
-    //     l_input.shed_row(dup_rows[r]);
-    //     longer_seqs.erase(dup_rows[r]);
-    // }
-    // Rcpp::NumericMatrix x = Rcpp::wrap(l_input); //convert back to rcpp matrix
-    // rownames(x) = longer_seqs;
-    // colnames(x) = colnames(long_input);
-    arma::mat M(dup_rows);
-    arma::vec X(first_dup_rows);
-    // arma:M.insert_cols(1,X);
-    arma::uvec indices = sort_index(M, "descend");
 
-    return indices;
+    arma::mat M(dup_rows);
+    arma::uvec indices = sort_index(M, "descend");
+    int dup_size = og_seqs.size();
+    for(int r=0; r<dup_size; ++r){ //remove data rows and duplicate taxa names
+        int index (indices[r]);
+        l_input.row(og_seqs[index]) = l_input.row(og_seqs[index]) + l_input.row(dup_rows[index]); //add values to original
+        l_input.shed_row(dup_rows[index]);
+        longer_seqs.erase(dup_rows[index]);
+    }
+    Rcpp::NumericMatrix x = Rcpp::wrap(l_input); //convert back to rcpp matrix
+    rownames(x) = longer_seqs;
+    colnames(x) = colnames(long_input);
+    
+    return x;
 }
 
 
--- compare_ASV_test/write_match.R
+++ compare_ASV_test/write_match.R
@@ -1,19 +1,32 @@
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 
-require(data.table)
-require(Rcpp)
+require(phyloseq);require(Rcpp)
+phyloseq_objects <- list(readRDS("~/Dropbox/manure_ml/compare_ASV_test/pitfoam.RDS"), readRDS("~/Dropbox/manure_ml/compare_ASV_test/nashua.RDS"))
+p_objects_names <- c("pitfoam", "nashua")
+names(phyloseq_objects) <- p_objects_names
 
-df1 <- asvs[[pairs[,run][1]]][1:6,1:10]
-df2 <- asvs[[pairs[,run][2]]][1:6,1:10]
+asvs <- lapply(phyloseq_objects, otu_table)
+read_size_order <- order(unlist(lapply(lapply(asvs, FUN = function(x){rownames(x)[1]}), FUN = function(seq){mean(length(strsplit(seq, "")[[1]]))})))
+asvs <- asvs[read_size_order]
 
-rownames(df1) <- c("abc", "def", "ghi", "jkl", "mno", "pqr")
-rownames(df2) <- c("abcd", "defg", "ghij", "abcm", "abcp", "pqrs")
+pairs <- combn(names(asvs), m = 2); run=1
+
+df1 <- asvs[[pairs[,run][1]]][1:12,1:10]
+df2 <- asvs[[pairs[,run][2]]][1:12,1:10]
+
+rownames(df1) <- c("abc", "def", "ghi", "jkl", "mno", "pqr","abc", "def", "ghi", "jkl", "mno", "pqr")
+rownames(df2) <- c("abcd", "defg", "ghij", "abcm", "abcp", "pqrs","aabcd", "adefg", "aghij", "aabcm", "aabcp", "apqrs")
 df1[1,] <- rep(1,10);df1[2,] <- rep(2,10);df1[3,] <- rep(3,10);df1[4,] <- rep(4,10);df1[5,] <- rep(5,10);df1[6,] <- rep(6,10)
+df1[7,] <- rep(1,10);df1[8,] <- rep(2,10);df1[9,] <- rep(3,10);df1[10,] <- rep(4,10);df1[11,] <- rep(5,10);df1[12,] <- rep(6,10)
 df2[1,] <- rep(1,10);df2[2,] <- rep(2,10);df2[3,] <- rep(3,10);df2[4,] <- rep(4,10);df2[5,] <- rep(5,10);df2[6,] <- rep(6,10)
+df2[7,] <- rep(1,10);df2[8,] <- rep(2,10);df2[9,] <- rep(3,10);df2[10,] <- rep(4,10);df2[11,] <- rep(5,10);df2[12,] <- rep(6,10)
 
 Sys.setenv("OMP_NUM_THREADS"="4")
 Sys.setenv("PKG_CXXFLAGS"=" -fopenmp")
+
+
 sourceCpp("compare_ASV_test/rcpp_seq_match_test.cpp");  test_match(df1, df2)
+
 test <- test_match(df1, df2)
 table(table(rownames(test))[table(rownames(test)) > 0])
 
