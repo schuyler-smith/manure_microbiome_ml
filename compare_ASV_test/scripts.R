fae3cb146f38866145e80d5b51f43c4aed2fe7ef
sdsmith1390 <sdsmith@iastate.edu>
2018-11-26 16:49:31 -0600

    complete reassign of taxa table and otu tables

--- compare_ASV_test/scripts.R
+++ compare_ASV_test/scripts.R
@@ -1,29 +1,40 @@
-# (...){
+merge_asvs <- function(...){
   options(warn=1)
   require(phyloseq);require(Rcpp);require(RcppParallel)
-  sourceCpp("compare_ASV_test/rcpp_seq_match.cpp")
+  sourceCpp("~/Dropbox/phyloseq_scripts/rcpp_seq_match.cpp")
   
   phyloseq_objects <- list(...)
   p_objects_names <- sapply(substitute(list(...))[-1], deparse)
 
-  phyloseq_objects <- list(readRDS("~/Dropbox/manure_ml/compare_ASV_test/pitfoam.RDS"), readRDS("~/Dropbox/manure_ml/compare_ASV_test/nashua.RDS"))
-  p_objects_names <- c("pitfoam", "nashua")
   names(phyloseq_objects) <- p_objects_names
 
   asvs <- lapply(phyloseq_objects, otu_table)
   read_size_order <- order(unlist(lapply(lapply(asvs, FUN = function(x){rownames(x)[1]}), FUN = function(seq){mean(length(strsplit(seq, "")[[1]]))})))
   asvs <- asvs[read_size_order]
-  
-  pairs <- combn(names(asvs), m = 2)
-  
-  for(run in 1:dim(pairs)[2]){
-    # asvs[[pairs[,run][2]]] <- match_seqs(asvs[[pairs[,run][1]]], asvs[[pairs[,run][2]]])
-    matched <- match_sequences(asvs[[pairs[,run][1]]], asvs[[pairs[,run][2]]])
 
-    tax_table(phyloseq_objects$pitfoam)[a]
-    # match_seqs(asvs[[pairs[,run][1]]][1:20], asvs[[pairs[,run][2]]][1:20])
+  pairs <- combn(names(asvs), m = 2)
+  for(run in 1:dim(pairs)[2]){
+    # Sys.setenv("OMP_NUM_THREADS"="8")
+    # Sys.setenv("PKG_CXXFLAGS"=" -fopenmp")
+    asvs[[pairs[,run][2]]] <- match_sequences(asvs[[pairs[,run][1]]], asvs[[pairs[,run][2]]])
   }
-  
+  all_taxa <- eval(parse(text=paste0("rbind(", paste0("tax_table(",p_objects_names,")", collapse = ", "), ")")))
   for(name in names(asvs)){
-    taxa_names(phyloseq_objects[[name]]) <- asvs[[name]]
+    otu_table(phyloseq_objects[[name]]) <- otu_table(asvs[[name]], taxa_are_rows = TRUE)
+    tax_table(phyloseq_objects[[name]]) <- all_taxa[rownames(all_taxa) %in% rownames(asvs[[name]]),]
   }
+  return(phyloseq_objects)
+}
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
